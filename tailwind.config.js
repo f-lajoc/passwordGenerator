c371a8d68d907c4bb50209b8e7bf0b969fd57336
lajoc_devs <bolajifisayo80@gmail.com>
2023-08-21 14:57:15 +0100

    building password generator

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./**/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				color1: "#ECFDF5",
+				color2: "#2B283A",
+				color3: "#10B981",
+				color4: "#6B7280",
+				color5: "#5DEF92",
+				color6: "#273549",
+				color7: "#1F2937",
+			},
+		},
+	},
+	plugins: [],
+};
+
