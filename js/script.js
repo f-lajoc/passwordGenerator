f6fa1cb84ea2df73b977004c627034fb2a187efb
lajoc_devs <fisayobolaji7@gmail.com>
2023-08-22 02:32:11 +0100

    done with styling

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-// self invoking function
+// self invoking function for light and dark mode
 (function () {
-  const darkToggle = document.querySelector('.toggle-dark');
+	const darkToggle = document.querySelector(".toggle-dark");
 
-
-// eventListener
-  darkToggle.addEventListener('click', (event) => {
-    event.preventDefault();
-    document.documentElement.classList.toggle('dark');
-    
-  })
-
+	// eventListener
+	darkToggle.addEventListener("click", (event) => {
+		event.preventDefault();
+		document.documentElement.classList.toggle("dark");
+	});
 })();
-//<i class="fa-solid fa-moon"></i>;
+
+const characters = [	"A",	"B",	"C", "D",  "E", "F",	"G",	"H",	"I",	"J","K",	"L",  "M", "N",	"O",	"P",	"Q","R",	"S",	"T",  "U", "V",	"W",	"X", "Y",	"Z",	"a","b",  "c", "d",	"e","f",	"g",	"h",	"i",	"j",  "k", "l","m",	"n",	"o",	"p",	"q","r", "s", "t", "u",	"v",	"w",	"x",	"y",	"z", "0",  "1",	"2",	"3",	"4",	"5",	"6","7", "8", "9",	"~",	"`",	"!",	"@","#","$","%", "^",  "&", "*",	"(",	")","_",	"-",	"+",	"=",	" {", "[",	"",	"",	",",	"",	":",	";",	"<",	 ">",	".",	"?",	"/",
+];
+
+console.log(characters[ Math.floor( Math.random() * characters.length)] );
