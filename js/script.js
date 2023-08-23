9b56bcaf626b866970061bb3734794c203810c4b
lajoc_devs <fisayobolaji7@gmail.com>
2023-08-23 14:50:19 +0100

    added dropdown menu and fetch DOM

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 // self invoking function for light and dark mode
-// (function () {
-// 	const darkToggle = document.querySelector(".toggle-dark");
+(function () {
+  const darkToggle = document.querySelector(".toggle-dark");
 
-// 	// eventListener
-// 	darkToggle.addEventListener("click", (event) => {
-// 		event.preventDefault();
-// 		document.documentElement.classList.toggle("dark");
-// 	});
-// })();
+  // eventListener
+  darkToggle.addEventListener("click", (event) => {
+    event.preventDefault();
+    document.documentElement.classList.toggle("dark");
+  });
+}) ();
 
 const characters = [	"A",	"B",	"C", "D",  "E", "F",	"G",	"H",	"I",	"J","K",	"L",  "M", "N",	"O",	"P",	"Q","R",	"S",	"T",  "U", "V",	"W",	"X", "Y",	"Z",	"a","b",  "c", "d",	"e","f",	"g",	"h",	"i",	"j",  "k", "l","m",	"n",	"o",	"p",	"q","r", "s", "t", "u",	"v",	"w",	"x",	"y",	"z", "0",  "1",	"2",	"3",	"4",	"5",	"6","7", "8", "9",	"~",	"`",	"!",	"@","#","$","%", "^",  "&", "*",	"(",	")","_",	"-",	"+",	"=",	" {", "[",	"",	"",	",",	"",	":",	";",	"<",	 ">",	".",	"?",	"/",
 ];
